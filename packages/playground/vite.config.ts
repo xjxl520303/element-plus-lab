8c682780a0930c1f42228e5cbb516b5793aee6a2
jenemy <xjxl520303@hotmail.com>
2026-02-22 15:57:08 +0800

    build(playground): 初始化演练场

---
+++ packages/playground/vite.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VineVitePlugin } from 'vue-vine/vite'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    VineVitePlugin(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
