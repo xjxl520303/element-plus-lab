905f20bff5ccfcacc247324e418eeb9e302ffd09
jenemy <xjxl520303@hotmail.com>
2026-03-04 18:31:38 +0800

    chore(lint-configs): 调整 eslint 的规则,避免 pnpm 下载包失败

--- internal/lint-configs/eslint-config/index.mjs
+++ internal/lint-configs/eslint-config/index.mjs
@@ -4,6 +4,10 @@ import globals from 'globals'
 
 export default antfu({
   // 重写 antfu 规则自身的配置
+  rules: {
+    // 避免在 pnpm-workspace.yaml 中强制添加 trustPolicy: downgrade 导致 pnpm install 时失败
+    'pnpm/yaml-enforce-settings': 'off',
+  },
 }, ...VueVine(), {
   languageOptions: {
     globals: {
